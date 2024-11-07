3056c6b7b0f0531549f6228a28f83c511ca90ea6
Ritam Bhattacharya <ritam07mc@gmail.com>
2024-11-07 18:58:30 +0530

    addded meta tags

--- Frontend/src/components/layout/layout.jsx
+++ Frontend/src/components/layout/layout.jsx
@@ -3,21 +3,30 @@ import Footer from './footer.jsx';
 import { Helmet } from 'react-helmet';
 
 // eslint-disable-next-line react/prop-types
-function Layout({ children }) {  // Destructure children from props
+function Layout({ children,title,description,keywords,author }) {  // Destructure children from props
   return (
     <div>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>My Title</title>
-        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content= {keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
       </Helmet>
       <Header />
       <main style={{ minHeight: '80vh' }}>
         {children}
       </main>
-      <Footer /> {/* Add Footer at the end */}
+      <Footer />  
     </div>
   );
 }
+
+Layout.defaultProps = {
+  title: 'Insta-Cart', 
+  description: 'THIS IS A ECOMERCE SITE', 
+  keywords: 'REACT NODE JS EXPRESS JS ', 
+  author: 'IEM STUDENTS'   
+};
 
 export default Layout;
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,11 +1,11 @@
-import Layout from '../components/layout/layout.jsx'
+import Layout from '../components/layout/layout.jsx';
 
 const Contact = () => {
   return (
-    <Layout>
+    <Layout title={"CONTACTS"}>
         <h1>Contact Us</h1>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
