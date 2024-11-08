5bae0bc4862ee896f1e090fa4fd51a1ec032c73a
Sudeshna <sudeshn0504@gmail.com>
2024-11-09 00:11:30 +0530

    images

---
+++ Frontend/src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/layout.jsx'
+
+const ProductDetails = () => {
+  return (
+    <Layout title={"Product Details"}>
+        <h1>Product Details</h1>
+    </Layout>
+  )
+}
+
+export default ProductDetails
