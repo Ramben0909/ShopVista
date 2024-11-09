a2840dcc1882cb0f6db3d23d5582959d8aa0cdb6
Rounak Chakraborty <rounak242003@gmail.com>
2024-11-10 01:45:02 +0530

    ahja

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,11 +1,28 @@
- import Layout from '../components/layout/layout.jsx'
+import Layout from '../components/layout/layout.jsx';
 
 const About = () => {
   return (
-    <Layout title={"About-us"}>
-        <h1> About Page</h1>
+    <Layout title={"About Us"}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are committed to providing the best service to our customers.
+      </p>
+      <section>
+        <h2>Our Mission</h2>
+        <p>
+          Our mission is to deliver quality products that exceed expectations and make a positive impact.
+        </p>
+      </section>
+      <section>
+        <h2>Our Values</h2>
+        <ul>
+          <li>Integrity</li>
+          <li>Innovation</li>
+          <li>Customer Satisfaction</li>
+        </ul>
+      </section>
     </Layout>
-  )
+  );
 }
 
-export default About
+export default About;
